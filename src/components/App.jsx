ba1709455cf486d9e7fd8c5ad66a18a1003d32b3
AlenaUshakova <alenushakova@gmail.com>
2023-04-13 12:18:25 +0300

    basic settings

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    ><h1>HELLO</h1>
+    </div>
+  );
+};
